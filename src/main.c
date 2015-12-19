dd9b0eec8f7fed91a2f29af04dc6bc58524743c2
Dorurung <wejof@jasodif.com>
2015-12-19 22:16:52 +0900

    Score Patched

--- src/main.c
+++ src/main.c
@@ -155,7 +155,7 @@ void touched(int x, int y)
 					scoretext[i].img = 18;
 					scoretext[i].x = 280 + 50*i;
 					scoretext[i].y = 80;
-					scene_additem(&scoretext[i]);
+					scene_additem(&(scoretext[i]));
 				}
 				scene_removeitem(&startbutton);
 				scene_removeitem(&currentscene);
@@ -181,7 +181,7 @@ void touched(int x, int y)
 			if(hitTest(300, 300, 200, 120, x, y, 0, 0)) {
 				gamestate = 0;
 				for(i = 0; i < 5; i++) {
-					scene_removeitem(&scoretext[i]);
+					scene_removeitem(&(scoretext[i]));
 				}
 				currentscene.img = 5;
 				scene_removeitem(&currentscene);
@@ -283,13 +283,13 @@ int main()
 				//drawing(mc.x, mc.y, height(mc.img), width(mc.img), img(mc.img));
 				//printf ("time: %d, score = %d\n", time, score);
 				printf("Debug State %d!\n", debug++);
-				int temp;
+				int temp, scores = score;
 				for(i = 4; i >= 0; i--) {
-					temp = score%10;
+					temp = scores%10;
 					if(!(temp == 0 && scoretext[i].img == 18)) {
 						scoretext[i].img = temp+8;
 					}
-					score /= 10;
+					scores /= 10;
 				}
 
 				printf("Debug State %d!\n", debug++);
